54353fbf3fd817490de796c257df010f12a4d2ff
step <236178296@qq.com>
2020-07-29 17:53:40 +0800

    myError 修复format时的参数错误

--- myError/error.go
+++ myError/error.go
@@ -30,16 +30,14 @@ func getWhere() string {
 
 func getFormat(format string, args []interface{}) string {
 	if len(args) > 0 {
-		format = fmt.Sprintf(format, args)
+		format = fmt.Sprintf(format, args...)
 	}
 	return format
 }
 
 func Warp(err error, format string, args ...interface{}) *Error {
 	var where string
-	if len(args) > 0 {
-		format = fmt.Sprintf(format, args)
-	}
+	format = getFormat(format, args)
 	switch t := err.(type) {
 	case *Error:
 		// 继承where
