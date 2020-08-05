699786ab26208d377f759f37e5d788ecbf2c87a6
step <236178296@qq.com>
2020-08-05 16:21:31 +0800

    增加TimeToDuration接口, 将时间转换为X秒, X分钟前的形式

--- timeTool/time.go
+++ timeTool/time.go
@@ -1,6 +1,16 @@
 package timeTool
 
-import "time"
+import (
+	"fmt"
+	"math"
+	"time"
+)
+
+const (
+	minute = 60
+	hour   = 3600
+	day    = 86400
+)
 
 // 获取当前的时间 - 字符串
 func GetCurrentDate() string {
@@ -20,4 +30,27 @@ func GetCurrentMilliUnix() int64 {
 // 获取当前的时间 - 纳秒级时间戳
 func GetCurrentNanoUnix() int64 {
 	return time.Now().UnixNano()
+}
+
+// 将时间转换为X秒, X分钟前, 这种形式
+func TimeToDuration(inTime time.Time) string {
+	timestamp := inTime.Unix()
+	return TimeStampToDuration(timestamp)
+}
+
+// 将时间转换为X秒, X分钟前, 这种形式
+func TimeStampToDuration(inTime int64) string {
+	now := time.Now().Unix()
+	seconds := now - inTime
+	if seconds < 0 {
+		return fmt.Sprintf("%d秒后", math.Abs(seconds))
+	} else if seconds < minute {
+		return fmt.Sprintf("%d秒前", seconds)
+	} else if seconds < hour {
+		return fmt.Sprintf("%d分钟前", int(seconds/minute))
+	} else if seconds < day {
+		return fmt.Sprintf("%d小时前", int(seconds/hour))
+	} else {
+		return fmt.Sprintf("%d天前", int(seconds/day))
+	}
 }
