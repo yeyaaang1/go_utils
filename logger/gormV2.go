91d0c7884509c42f7841a6f7a7a409537aece9cd
周涛 <236178296@qq.com>
2020-09-08 11:19:44 +0800

    优化gorm V2 logger

--- logger/gormV2.go
+++ logger/gormV2.go
@@ -48,8 +48,8 @@ func (dbLogger *dbLogger) Error(ctx context.Context, msg string, data ...interfa
 }
 
 const (
-	DBFmtWithError   = "\033[36;1m[%.2fms]\033[0m \033[36;31m[%d rows affected or returned]\033[0m \n%s\n%s\n\033[31;1m%s\033[0m\n"
-	DBFmtWithNoError = "\n\033[36;1m[%.2fms]\033[0m \033[36;31m[%d rows affected or returned]\033[0m \n%s\n%s\n"
+	DBFmtWithError   = "%s\n\033[36;1m[%.2fms]\033[0m %s\n\033[36;31m[%d rows affected or returned]\033[0m \n\u001B[31;1m%s\u001B[0m"
+	DBFmtWithNoError = "%s\n\033[36;1m[%.2fms]\033[0m %s\n\033[36;31m[%d rows affected or returned]\033[0m"
 )
 
 func (dbLogger *dbLogger) Trace(ctx context.Context, begin time.Time, fc func() (string, int64), err error) {
@@ -58,11 +58,11 @@ func (dbLogger *dbLogger) Trace(ctx context.Context, begin time.Time, fc func() 
 	sql, rows, msElapsed := dbLogger.getSqlInfo(fc, elapsed)
 	switch {
 	case err != nil && dbLogger.LogLevel >= gormLogger.Error:
-		golog.Default.Errorf(DBFmtWithError, msElapsed, rows, sql, file, err.Error())
+		golog.Default.Errorf(DBFmtWithError, file, msElapsed, sql, rows, err.Error())
 	case elapsed > dbLogger.SlowThreshold && dbLogger.SlowThreshold != 0 && dbLogger.LogLevel >= gormLogger.Warn:
-		golog.Default.Warnf(DBFmtWithNoError, msElapsed, rows, sql, file)
+		golog.Default.Warnf(DBFmtWithNoError, file, msElapsed, sql, rows)
 	case dbLogger.LogLevel >= gormLogger.Info:
-		golog.Default.Infof(DBFmtWithNoError, msElapsed, rows, sql, file)
+		golog.Default.Infof(DBFmtWithNoError, file, msElapsed, sql, rows)
 	}
 	if dbLogger.callback != nil {
 		dbLogger.callback(sql, file, rows, msElapsed)
