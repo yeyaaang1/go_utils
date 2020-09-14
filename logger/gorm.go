5318d75f0a9608ce1985be0b3d2894ece9a69eea
周涛 <236178296@qq.com>
2020-09-14 14:30:47 +0800

    调试dbLogger

--- logger/gorm.go
+++ logger/gorm.go
@@ -18,23 +18,24 @@ var (
 type MyDBLogger struct {
 	SuccessCallback func(sql string, source string, rows int, duration float64)
 	FailCallback    func(source string, msg ...interface{})
+	Logout          bool
 }
 
 func (logger *MyDBLogger) Print(values ...interface{}) {
 	// fmt.Println(fmt.Sprintf("%+v", values))
+	fmt.Println(fmt.Sprintf("%v", values))
 	var (
 		level       = values[0]
-		currentTime = "\n\033[33m[" + time.Now().Format("2006-01-02 15:04:05") + "]\033[0m"
+		currentTime = "\033[33m[" + time.Now().Format("2006-01-02 15:04:05") + "]\033[0m"
 		messages    = []interface{}{fmt.Sprintf("\033[35m(%v)\033[0m", values[1]), currentTime}
 	)
 	if level == "sql" {
-		// fmt.Println(sql, source, values[5], values[2])
 		// 构建原始的控制台输出
 		duration := float64(values[2].(time.Duration).Nanoseconds()/1e4) / 100.0
 		messages = append(messages, fmt.Sprintf(" \033[36;1m[%.2fms]\033[0m ", duration))
 		sql := sqrFormat(values[3].(string), values[4].([]interface{}))
 		messages = append(messages, sql)
-		messages = append(messages, fmt.Sprintf(" \n\033[36;31m[%v]\033[0m ", strconv.FormatInt(values[5].(int64), 10)+" rows affected or returned "))
+		messages = append(messages, fmt.Sprintf(" \n\033[36;31m[%v]\033[0m ", strconv.FormatInt(values[5].(int64), 10)+" rows affected or returned"))
 		if logger.SuccessCallback != nil {
 			rows := int(values[5].(int64))
 			source := fmt.Sprintf("%v", values[1])
@@ -49,7 +50,9 @@ func (logger *MyDBLogger) Print(values ...interface{}) {
 			logger.FailCallback(source, values[2:]...)
 		}
 	}
-	fmt.Println(messages...)
+	if logger.Logout {
+		fmt.Println(messages...)
+	}
 }
 
 func sqrFormat(inSql string, args []interface{}) (sql string) {
